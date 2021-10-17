9756a5b5181ff6014acbf66aabdaf5416f88529b
elzimoch <24Elwira62!>
2021-10-17 22:36:29 +0200

    Weather_App 1.0

--- App.js
+++ App.js
@@ -1,21 +1,86 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import * as Location from 'expo-location'
+import WeatherInfo from './components/WeatherInfo'
+import UnitsPicker from './components/UnitsPicker'
+import {colors} from './utils/index'
+import ReloadIcon from './components/ReloadIcon'
+
+const WEATHER_API_KEY = 'a14bc5d0125ed104de202d314d6cf676'
+const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [currentWeather, setCurrentWeather] = useState(null)
+  const [unitsSystem, setUnitsSystem] = useState('metric')
+
+  useEffect(() => {
+    load()
+  }, [unitsSystem])
+
+  async function load() {
+    setCurrentWeather(null)
+    setErrorMessage(null)
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status != 'granted') {
+        setErrorMessage('Access to location is needed to run the app')
+        return
+      }
+      const location = await Location.getCurrentPositionAsync()
+      const {latitude, longitude} = location.coords
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`
+      
+      const response = await fetch(weatherUrl)
+      const result = await response.json()
+      if(response.ok) {
+        setCurrentWeather(result)
+      } else {
+        setErrorMessage(response.errorMessage)
+      }
+
+    } catch (error) {
+      setErrorMessage(error.errorMessage)
+    }
+  }
+  if (currentWeather) {
+      return (
+        <View style={styles.container}>
+          <StatusBar style="auto" />
+          <View style={styles.main}>
+            <UnitsPicker unitsSystem={unitsSystem} setUnitsSystem={setUnitsSystem}/>
+            <ReloadIcon load={load}/>
+            <WeatherInfo currentWeather={currentWeather}/>
+          </View>
+        </View>
+      );
+  } else if (errorMessage) {
+    return (
+      <View style={styles.container}>
+        <Text>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color={colors.SECONDARY_COLOR}/>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'pink',
     justifyContent: 'center',
   },
+  main: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center'
+  }
 });
---
+++ utils/index.js
@@ -0,0 +1,5 @@
+export const colors = {
+    PRIMARY_COLOR: '#ffb6c1',
+    SECONDARY_COLOR: '#778899',
+    BORDER_COLOR: '#dbdbdb'
+}
